5da3c2c25461ac4d40e19e091a64d741ae81dbc8
Andrew Peliza <andrewpeliza@hotmail.com>
2022-12-14 13:52:01 +0000

    feat: two simple api's, movie model and go-chi package added

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -20,10 +20,8 @@ func main() {
 
 	log.Println("Starting application on port", port)
 
-	http.HandleFunc("/", Hello)
-
 	// start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
--- server/cmd/api/routes.go
+++ server/cmd/api/routes.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	// create a router mux
+	mux := chi.NewRouter()
+
+	// middleware
+	mux.Use(middleware.Recoverer)
+
+	// handlers
+	mux.Get("/", app.Home)
+
+	mux.Get("/movies", app.AllMovies)
+
+	return mux
+}
